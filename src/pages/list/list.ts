fe2aee52312255cd3e43e33bad0ebb22cd4cf692
melanierosson <melanie.rosson@gmail.com>
2017-05-13 14:32:24 -0700

    list

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -9,19 +9,16 @@ import { ItemDetailsPage } from '../item-details/item-details';
   templateUrl: 'list.html'
 })
 export class ListPage {
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  subjects: string[];
+  items: Array<{title: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+    this.subjects = ['Geometry', 'Calculus', 'Biology', 'Programming', 'Statistics', 'Chemistry', 'Psychology', 'English', 'ESL', 'History'];
 
     this.items = [];
     for(let i = 1; i < 11; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+        title: this.subjects[i]
       });
     }
   }
